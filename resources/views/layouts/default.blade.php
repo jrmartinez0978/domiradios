9534e85f71ec0e78092989b42cd9f619ef87726d
jose <jrmartinez0978@gmail.com>
2024-09-11 17:01:06 -0400

    ok

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -38,7 +38,7 @@
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <h1 class="text-3xl font-bold text-gray-900">
                 <a href="{{ url('/') }}" class="hover:text-blue-500">
-                    @yield('header', 'Directorio de Radios')
+                    @yield('header', 'Domiradios | Emisoras Dominicanas')
                 </a>
             </h1>
         </div>
