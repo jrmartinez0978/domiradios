04053fc580636c0cca7491224d503d92ddf9f956
jose <jrmartinez0978@gmail.com>
2024-09-11 00:30:25 -0400

    ok

--- resources/views/detalles.blade.php
+++ resources/views/detalles.blade.php
@@ -61,17 +61,17 @@
     </div>
 
     <!-- Emisoras relacionadas -->
-    <div class="mt-8">
-        <h2 class="text-lg font-semibold mb-4">Otras emisoras de {{ $radio->genres->pluck('name')->implode(', ') }}</h2>
-        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 mt-4">
-            @foreach($relatedRadios as $related)
-                <div class="bg-white p-4 rounded-lg shadow-md">
-                    <a href="{{ route('emisoras.show', $related->slug) }}">
-                        <img src="{{ Storage::url($related->img) }}" alt="{{ $related->name }}" class="w-full h-auto rounded-md mb-4">
-                        <h3 class="text-xl font-bold">{{ $related->name }}</h3>
-                    </a>
-                </div>
-            @endforeach
+<div class="mt-8">
+    <h2 class="text-lg font-semibold mb-4">Otras emisoras de {{ $radio->genres->pluck('name')->implode(', ') }}</h2>
+    <div class="grid grid-cols-4 sm:grid-cols-6 lg:grid-cols-8 gap-6 mt-4">
+        @foreach($relatedRadios as $related)
+            <div class="bg-white p-4 rounded-lg shadow-md">
+                <a href="{{ route('emisoras.show', $related->slug) }}">
+                    <img src="{{ Storage::url($related->img) }}" alt="{{ $related->name }}" class="w-full h-auto rounded-md mb-4">
+                    <h3 class="text-xl font-bold">{{ $related->name }}</h3>
+                </a>
+            </div>
+        @endforeach
         </div>
     </div>
 </div>
