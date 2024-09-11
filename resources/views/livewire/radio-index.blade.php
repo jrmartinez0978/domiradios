2f619d2cb6d57547fa621dc336d8685d594406d6
jose <jrmartinez0978@gmail.com>
2024-09-11 19:31:28 -0400

    ok

--- resources/views/livewire/radio-index.blade.php
+++ resources/views/livewire/radio-index.blade.php
@@ -27,8 +27,7 @@
             </h2>
             <p class="text-sm text-gray-600">Frecuencia: {{ $radio->bitrate }}</p>
             <p class="text-sm text-gray-600">Ciudad: {{ $radio->genres->pluck('name')->implode(', ') }}</p>
-            <p class="text-sm text-gray-600">Género: {{ $radio->tags }}</p>
-
+            <p class="text-sm text-gray-600">Género: {{ explode(',', $radio->tags)[0] }}</p>
 
                     <!-- Reproductor de audio con botón único Play/Stop -->
                     <div class="mt-4">
