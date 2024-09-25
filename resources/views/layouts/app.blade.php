71879239b11226b01e8a60dc59ffb504bf763253
jose <jrmartinez0978@gmail.com>
2024-09-25 11:37:54 -0400

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -26,7 +26,7 @@
     <meta name="twitter:image" content="@yield('twitter_image', asset('images/default-twitter-image.jpg'))" />
 
     <!-- Styles -->
-    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    @vite('resources/css/app.css') <!-- Incluir el CSS compilado de Tailwind -->
      <!-- Include Font Awesome -->
      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
 
@@ -71,6 +71,7 @@
     </footer>
 
     @livewireScripts
+    @vite('resources/js/app.js') <!-- Incluir el JavaScript compilado -->
 
 </body>
 </html>
